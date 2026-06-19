4a042f42e36640830f0fbc46a3218f2edb65740e
Mohit Raghav <mohitraghav1318@gmail.com>
2026-06-19 13:31:34 +0530

    About Us

--- Frontend/src/features/landing/pages/AboutUs.jsx
+++ Frontend/src/features/landing/pages/AboutUs.jsx
@@ -1,16 +1,25 @@
 import LandingNavbar from "../components/LandingNavbar";
 import Footer from "../components/Footer";
+import AboutHero from "../components/about/AboutHero";
+import AboutMission from "../components/about/AboutMission";
+import AboutValues from "../components/about/AboutValues";
+import AboutTeam from "../components/about/AboutTeam";
+import AboutStats from "../components/about/AboutStats";
+
+import "../style/about.scss";
 
 const AboutUs = () => {
-    return (
-        <>
-        <LandingNavbar />
-        <div>
-            <h1>About Us</h1>
-        </div>
-        <Footer />
-        </>
-    )
-}
+  return (
+    <div className="about-page">
+      <LandingNavbar />
+      <AboutHero />
+      <AboutMission />
+      <AboutValues />
+      <AboutTeam />
+      <AboutStats />
+      <Footer />
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
